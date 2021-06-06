329495c93f2b368ffb651dc18a1f268129713a2b
Naoya Ikeda <naoya.ikeda@exchange.grayrecord.com>
2021-06-06 10:06:50 +0900

    Visualize of Phython

---
+++ viz.py
@@ -0,0 +1,11 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# %%
+output_fit_0 = np.loadtxt('output_fit_0', delimiter = ' ')
+output_fit_1 = np.loadtxt('output_fit_1', delimiter = ' ')
+output_mut_0 = np.loadtxt('output_mut_0', delimiter = ' ')
+output_mut_1 = np.loadtxt('output_mut_1', delimiter = ' ')
